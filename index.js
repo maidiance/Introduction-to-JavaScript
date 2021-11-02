92a5dfb1384b343685dfcab60b1b8604933cfe7c
Winifred Lu <winnie.lu@comcast.net>
2021-11-02 10:49:47 -0700

    task 2 complete

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
 
 
